08977e823171caf695d0cfc5a74567a2fe3ee23e
Manuel Mendoza <manuelmendoza@uvigo.es>
2021-11-26 06:37:54 +0100

    checking if the modules are installed

--- analysis/check_dep.py
+++ analysis/check_dep.py
@@ -43,4 +43,10 @@ modules_required = [
 ]
 for mod in modules_required:
     resmod.update({mod: check_depmod(mod)})
-print(resmod)
+
+if not all(list(resmod.values())):
+    print(tnow() + " FAIl: Some module is not installed. Please check the dependencies")
+    for k in list(resmod.keys()):
+        if not resmod[k]:
+            print(tnow() + " FAIL: " + k + " is not installed")
+    raise ModuleNotFoundError

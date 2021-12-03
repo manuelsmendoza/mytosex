9bef8fa1dc0f292b8341e9c76753c0bfbc5ca723
Manuel Mendoza <manuelmendoza@uvigo.es>
2021-12-03 05:13:29 +0100

    remove command printing

--- src/func_analysis.py
+++ src/func_analysis.py
@@ -134,7 +134,6 @@ def filter_alignment(alignment, threads, layout, require=None, exclude=None):
                   + sample_prefix + ".fixmate.bam " \
                   + sample_prefix + ".bam"
     for cmd in [filter_mapped, group_names, fixmate, sort_position, deduplicate]:
-        print(cmd, file=sys.stdout)
         out = sp.run(
             cmd,
             shell=True,

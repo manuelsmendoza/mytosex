7260732601f569925e6508bc6e027fe5cb8b523c
Manuel Mendoza <manuelmendoza@uvigo.es>
2021-12-07 03:12:02 +0100

    uncoment required lines

--- run/analysis.py
+++ run/analysis.py
@@ -91,18 +91,18 @@ metrics_list = []
 results_list = []
 for sample in list(settings["samples"].keys()):
     print(tnow() + " INFO: Filtering the alignments of " + settings["samples"][sample]["alias"], file=sys.stdout)
-    if settings["samples"][sample]["layout"] == "paired":
-        filter_alignment(
-            alignment=os.path.join(tmp_dir, settings["samples"][sample]["alias"] + ".sam"),
-            threads=settings["numb_threads"],
-            layout="paired"
-        )
-    elif settings["samples"][sample]["layout"] == "single":
-        filter_alignment(
-            alignment=os.path.join(tmp_dir, settings["samples"][sample]["alias"] + ".sam"),
-            threads=settings["numb_threads"],
-            layout="single"
-        )
+    # if settings["samples"][sample]["layout"] == "paired":
+    #     filter_alignment(
+    #         alignment=os.path.join(tmp_dir, settings["samples"][sample]["alias"] + ".sam"),
+    #         threads=settings["numb_threads"],
+    #         layout="paired"
+    #     )
+    # elif settings["samples"][sample]["layout"] == "single":
+    #     filter_alignment(
+    #         alignment=os.path.join(tmp_dir, settings["samples"][sample]["alias"] + ".sam"),
+    #         threads=settings["numb_threads"],
+    #         layout="single"
+    #     )
 
     print(tnow() + " INFO: Extracting alignment statistics of " + settings["samples"][sample]["alias"], file=sys.stdout)
     extract_stats(

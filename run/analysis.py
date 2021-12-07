0dc652e2edf565b252e26bdc1aca8fdc7b10cdc4
Manuel Mendoza <manuelmendoza@uvigo.es>
2021-12-07 03:09:50 +0100

    uncoment required lines

--- run/analysis.py
+++ run/analysis.py
@@ -85,47 +85,47 @@ for ext in [".fasta", ".gff", ".bed"]:
 #             sreads=settings["samples"][sample]["single"]
 #         )
 
-# Filter the alignments and perform sex prediction (also extract more stats)
-# metrics_list = []
-# results_list = []
-# for sample in list(settings["samples"].keys()):
-#     print(tnow() + " INFO: Filtering the alignments of " + settings["samples"][sample]["alias"], file=sys.stdout)
-#     if settings["samples"][sample]["layout"] == "paired":
-#         filter_alignment(
-#             alignment=os.path.join(tmp_dir, settings["samples"][sample]["alias"] + ".sam"),
-#             threads=settings["numb_threads"],
-#             layout="paired"
-#         )
-#     elif settings["samples"][sample]["layout"] == "single":
-#         filter_alignment(
-#             alignment=os.path.join(tmp_dir, settings["samples"][sample]["alias"] + ".sam"),
-#             threads=settings["numb_threads"],
-#             layout="single"
-#         )
-#
-#     print(tnow() + " INFO: Extracting alignment statistics of " + settings["samples"][sample]["alias"], file=sys.stdout)
-#     extract_stats(
-#         alignment=os.path.join(tmp_dir, settings["samples"][sample]["alias"] + ".markdup.bam"),
-#         features=os.path.join(tmp_dir, settings["reference"]["alias"] + ".bed"),
-#         threads=settings["numb_threads"]
-#     )
-#
-#     sample_stats = alignment_stats(
-#             coverage=os.path.join(tmp_dir, settings["samples"][sample]["alias"] + ".cov.tsv"),
-#             feat_coverage=os.path.join(tmp_dir, settings["samples"][sample]["alias"] + ".bedcov.tsv"),
-#             depth=os.path.join(tmp_dir, settings["samples"][sample]["alias"] + ".depth.tsv"),
-#             salias=settings["samples"][sample]["alias"],
-#             ralias=settings["reference"]["alias"]
-#         )
-#     metrics_list.append(sample_stats)
-#
-# print(tnow() + " INFO: Exporting alignment statistics", file=sys.stdout)
-# align_metrics = pd.concat(metrics_list)
-# align_metrics.to_csv(
-#      os.path.join(data_dir, "align_stats.tsv"),
-#      sep="\t",
-#      index=False
-# )
+Filter the alignments and perform sex prediction (also extract more stats)
+metrics_list = []
+results_list = []
+for sample in list(settings["samples"].keys()):
+    print(tnow() + " INFO: Filtering the alignments of " + settings["samples"][sample]["alias"], file=sys.stdout)
+    if settings["samples"][sample]["layout"] == "paired":
+        filter_alignment(
+            alignment=os.path.join(tmp_dir, settings["samples"][sample]["alias"] + ".sam"),
+            threads=settings["numb_threads"],
+            layout="paired"
+        )
+    elif settings["samples"][sample]["layout"] == "single":
+        filter_alignment(
+            alignment=os.path.join(tmp_dir, settings["samples"][sample]["alias"] + ".sam"),
+            threads=settings["numb_threads"],
+            layout="single"
+        )
+
+    print(tnow() + " INFO: Extracting alignment statistics of " + settings["samples"][sample]["alias"], file=sys.stdout)
+    extract_stats(
+        alignment=os.path.join(tmp_dir, settings["samples"][sample]["alias"] + ".markdup.bam"),
+        features=os.path.join(tmp_dir, settings["reference"]["alias"] + ".bed"),
+        threads=settings["numb_threads"]
+    )
+
+    sample_stats = alignment_stats(
+            coverage=os.path.join(tmp_dir, settings["samples"][sample]["alias"] + ".cov.tsv"),
+            feat_coverage=os.path.join(tmp_dir, settings["samples"][sample]["alias"] + ".bedcov.tsv"),
+            depth=os.path.join(tmp_dir, settings["samples"][sample]["alias"] + ".depth.tsv"),
+            salias=settings["samples"][sample]["alias"],
+            ralias=settings["reference"]["alias"]
+        )
+    metrics_list.append(sample_stats)
+
+print(tnow() + " INFO: Exporting alignment statistics", file=sys.stdout)
+align_metrics = pd.concat(metrics_list)
+align_metrics.to_csv(
+     os.path.join(data_dir, "align_stats.tsv"),
+     sep="\t",
+     index=False
+)
 
 
 print(tnow() + " INFO: Building the neural network", file=sys.stdout)

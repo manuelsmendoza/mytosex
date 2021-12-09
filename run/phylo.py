2a915d2d16c9ade84eef48c92b81461bffe87fba
Manuel Mendoza <manuelmendoza@uvigo.es>
2021-12-09 10:06:26 +0100

    cds annotation added

--- run/phylo.py
+++ run/phylo.py
@@ -9,17 +9,17 @@ settings = load_settings(os.getenv("MYTOSEX_SETTINGS"))
 tmp_dir = os.path.join(settings["output_dir"], "tmp")
 
 # Create a local database to annotate the genes
-print(tnow() + " INFO: Creating a local database to annotate the genes", file=sys.stdout)
-extract_cds(
-    sequence=os.path.join(tmp_dir, settings["reference"]["alias"] + ".fasta"),
-    annotation=os.path.join(tmp_dir, settings["reference"]["alias"] + ".gff"),
-    outdir=tmp_dir,
-    alias=settings["reference"]["alias"]
-)
-create_db(
-    sequences=os.path.join(tmp_dir, settings["reference"]["alias"] + "_cds.fasta"),
-    outdir=tmp_dir
-)
+# print(tnow() + " INFO: Creating a local database to annotate the genes", file=sys.stdout)
+# extract_cds(
+#     sequence=os.path.join(tmp_dir, settings["reference"]["alias"] + ".fasta"),
+#     annotation=os.path.join(tmp_dir, settings["reference"]["alias"] + ".gff"),
+#     outdir=tmp_dir,
+#     alias=settings["reference"]["alias"]
+# )
+# create_db(
+#     sequences=os.path.join(tmp_dir, settings["reference"]["alias"] + "_cds.fasta"),
+#     outdir=tmp_dir
+# )
 
 # Assemble the transcriptomes and extract the CDS
 for sample in list(settings["samples"].keys()):
@@ -33,30 +33,35 @@ for sample in list(settings["samples"].keys()):
     #     threads=settings["numb_threads"]
     # )
 
-    print(tnow() + " INFO: Assembling the mitogenome of " + sname, file=sys.stdout)
-    if settings["samples"][sample]["layout"] == "single":
-        transcripts_assembly(
-            layout="single",
-            sreads=os.path.join(tmp_dir, settings["samples"][sample]["alias"] + ".fasta.gz"),
-            alignment=os.path.join(tmp_dir, settings["samples"][sample]["alias"] + ".markdup.bam"),
-            outdir=tmp_dir,
-            threads=settings["numb_threads"],
-            maxmem=settings["max_memory"],
-            alias=settings["samples"][sample]["alias"]
-        )
-    elif settings["samples"][sample]["layout"] == "paired":
-        transcripts_assembly(
-            layout="paired",
-            freads=os.path.join(tmp_dir, settings["samples"][sample]["alias"] + "_1.fasta.gz"),
-            rreads=os.path.join(tmp_dir, settings["samples"][sample]["alias"] + "_2.fasta.gz"),
-            alignment=os.path.join(tmp_dir, settings["samples"][sample]["alias"] + ".markdup.bam"),
-            outdir=tmp_dir,
-            threads=settings["numb_threads"],
-            maxmem=settings["max_memory"],
-            alias=settings["samples"][sample]["alias"]
-        )
+    # print(tnow() + " INFO: Assembling the mitogenome of " + sname, file=sys.stdout)
+    # if settings["samples"][sample]["layout"] == "single":
+    #     transcripts_assembly(
+    #         layout="single",
+    #         sreads=os.path.join(tmp_dir, settings["samples"][sample]["alias"] + ".fasta.gz"),
+    #         alignment=os.path.join(tmp_dir, settings["samples"][sample]["alias"] + ".markdup.bam"),
+    #         outdir=tmp_dir,
+    #         threads=settings["numb_threads"],
+    #         maxmem=settings["max_memory"],
+    #         alias=settings["samples"][sample]["alias"]
+    #     )
+    # elif settings["samples"][sample]["layout"] == "paired":
+    #     transcripts_assembly(
+    #         layout="paired",
+    #         freads=os.path.join(tmp_dir, settings["samples"][sample]["alias"] + "_1.fasta.gz"),
+    #         rreads=os.path.join(tmp_dir, settings["samples"][sample]["alias"] + "_2.fasta.gz"),
+    #         alignment=os.path.join(tmp_dir, settings["samples"][sample]["alias"] + ".markdup.bam"),
+    #         outdir=tmp_dir,
+    #         threads=settings["numb_threads"],
+    #         maxmem=settings["max_memory"],
+    #         alias=settings["samples"][sample]["alias"]
+    #     )
     
-    # print(tnow() + " INFO: Annotating the genes of " + settings["samples"][sample]["alias"])
-    # annotate_cds(
-    #     codseq=os.path.join(tmp_dir, settings["samples"][sample]["alias"] + "_trinity", )
-    # )
+    print(tnow() + " INFO: Annotating the genes of " + settings["samples"][sample]["alias"])
+    wd = os.path.join(tmp_dir, settings["samples"][sample]["alias"] + "_trinity")
+    annotate_cds(
+        codseq=os.path.join(wd, "Trinity-GG.fasta.transdecoder.cds"),
+        database=os.path.join(tmp_dir, settings["reference"]["alias"] + "_cds"),
+        outdir=tmp_dir,
+        alias=settings["samples"][sample]["alias"],
+        threads=settings["numb_threads"]
+    )

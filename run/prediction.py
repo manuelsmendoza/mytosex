4c7f51e89b324ede8ecce1e06a34f0dc79ca5021
Manuel Mendoza <manuelmendoza@uvigo.es>
2021-12-16 11:25:03 +0100

    round the prediction index

--- run/prediction.py
+++ run/prediction.py
@@ -53,7 +53,7 @@ samples_info = align_metrics.loc[:, ["mtfcov", "mtmcov", "mtfmd", "mtmmd", "mtfg
 sex_prediction = model.predict(samples_info)
 pred_value = []
 for pred in sex_prediction:
-    print(pred)
+    print(pred[0])
 #sex_prediction = [int(x.round()) for x in sex_prediction]
 
 results = {"sample": samples_name, "sex": sex_prediction}
